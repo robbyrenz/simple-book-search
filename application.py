3dffd1d38e2eaf6192910d79bae48c868c6555bd
Robby Renz <nxgrenz@gmail.com>
2019-09-10 19:41:47 -0500

    search functionality is now somewhat case-insensitive

--- application.py
+++ application.py
@@ -16,7 +16,7 @@ def index():
 
 @app.route("/results", methods=["POST"])
 def results():
-    book = request.form.get("book")
+    book = request.form.get("book").title()
     books = db.execute(f"SELECT * FROM books WHERE title LIKE '%{book}%'").fetchall()
     if books is None:  # TODO: this is not working!
         return render_template("results.html", books="Your search came up empty!!!")
